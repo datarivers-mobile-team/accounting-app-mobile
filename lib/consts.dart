482f013526bf443dd098b8388f6e6340bd7ed26e
hosseinEnzo <hosseinmoosavi97@gmail.com>
2022-09-14 19:42:53 +0430

    splash screen added

---
+++ lib/consts.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ConstValue {
+  static const kPurple = Color(0xff9067A9);
+  static const kYellowWhite = Color(0xffFFF0D1);
+}
--- lib/lib/presentation/pages/home/splash_screen.dart
+++ lib/lib/presentation/pages/home/splash_screen.dart
@@ -1,3 +1,4 @@
+import 'package:accounting_app_mobile/consts.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -5,6 +6,82 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [ConstValue.kPurple, ConstValue.kYellowWhite],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter),
+            ),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    "assets/images/PNG/splash_logo.png",
+                    width: 150,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Accounting",
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            top: 0,
+            right: 0,
+            child: Stack(
+              alignment: Alignment.topRight,
+              children: [
+                Image.asset(
+                  "assets/images/PNG/splash_top2.png",
+                  width: 240,
+                ),
+                Image.asset(
+                  "assets/images/PNG/splash_top1.png",
+                  width: 200,
+                ),
+                Image.asset(
+                  "assets/images/PNG/splash_top0.png",
+                  width: 200,
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            child: Stack(
+              alignment: Alignment.bottomLeft,
+              children: [
+                Image.asset(
+                  "assets/images/PNG/splash_bottom1.png",
+                  width: 210,
+                ),
+                Image.asset(
+                  "assets/images/PNG/splash_bottom2.png",
+                  width: 200,
+                ),
+                Positioned(
+                  left: 40,
+                  child: Image.asset(
+                    "assets/images/PNG/splash_bottom3.png",
+                    width: 200,
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
